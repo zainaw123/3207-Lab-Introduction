2fc67f209faa4b7bab6a4ac2d9697fcb3e9d6401
zainaw123 <123004710+zainaw123@users.noreply.github.com>
2023-01-21 14:42:00 -0500

    Add files via upload

---
+++ myheader.h
@@ -0,0 +1,10 @@
+//
+// Created by Zaina on 1/18/2023.
+//
+
+#ifndef INC_3207_LAB_INTRODUCTION_MYHEADER_H
+#define INC_3207_LAB_INTRODUCTION_MYHEADER_H
+char randchar();
+
+
+#endif //INC_3207_LAB_INTRODUCTION_MYHEADER_H
